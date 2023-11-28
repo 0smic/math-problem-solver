cae0aaeff1f863044049d8a143b436e96bbbcb11
Gokul <146622216+huho-0@users.noreply.github.com>
2023-11-28 16:54:29 +0530

    added more func for code readability

--- main.c
+++ main.c
@@ -1,37 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
-    /*This section help you to calcualte the root of the qudratic eqn*/
-    int a;
-    int b;
-    int c;
-    printf("In this Program we going to find the root of a quradatic eqn \n");
-    printf("Enter the coefficent of x^2: ");
-    scanf("%d", &a);
-    printf("Enter the cofficent of x: ");
-    scanf("%d", &b);
-    printf("Enter the constant Term: ");
-    scanf("%d", &c);
-    printf("Enter terms a: %d b : %d  c: %d", a, b, c);
-    double dis = pow(b, 2) - (4 * a * c);
-    if (dis>=0){
-        double root1 = (-b + sqrt(dis))/(2*a);
-        double root2 = (-b - sqrt(dis))/(2*a);
-        printf("Aplha is = %f \n", root1);
-        printf("Beta is = %f \n", root2);
-        printf("Hope you enjoyed it ....");
-        
-    } else if (dis<0){
-        printf("\nThe Discrimant is Non-Positive\n");
-        printf("So the roots are complex");
-        
-    }
-
-    /*This section help to find the determinant of the martix 2by2 and 3by3*/
+void matrix(){
     int select;
-    printf("\nEnter 2 for 2by2 and 3 for 3by3 Matrix: ");
+    printf("Format of Matrix to Find Determinant\n");
+    printf("Type 2 for 2by2\n");
+    printf("Type 3 for 3by3\n");
+    printf("Enter the No. corresponding: ");
     scanf("%d", &select);
     if (select==2){
         int a11;
@@ -53,11 +28,7 @@ int main()
         printf("| %d %d |\n", a21, a22);
         int det = (a11 * a22) - (a21 * a12);
         printf("Determinant of Your matrix = %d", det);
-    
-    }
-
-
-    if (select=3){
+    }else if (select==3){
         int a11;
         int a12;
         int a13;
@@ -94,7 +65,47 @@ int main()
         printf("| %d %d %d |\n", a31, a32, a33);
         int det = ((a11 * a22 * a33) + (a12 * a23 * a31) + (a11 * a22 * a32) ) - ((a31 * a22 * a13) + (a32 * a23 * a11) + (a33 * a21 * a12));
         printf("Determinant of Your Matrix = %d", det);
-    
-
+    }
+}
+void root_of_quadraic() {
+    int a;
+    int b;
+    int c;
+    printf("In this Program we going to find the root of a quradatic eqn \n");
+    printf("Enter the coefficent of x^2: ");
+    scanf("%d", &a);
+    printf("Enter the cofficent of x: ");
+    scanf("%d", &b);
+    printf("Enter the constant Term: ");
+    scanf("%d", &c);
+    printf("Enter terms a: %d b : %d  c: %d", a, b, c);
+    double dis = pow(b, 2) - (4 * a * c);
+    if (dis>=0){
+        double root1 = (-b + sqrt(dis))/(2*a);
+        double root2 = (-b - sqrt(dis))/(2*a);
+        printf("\nAplha is = %f \n", root1);
+        printf("Beta is = %f \n", root2);
+        
+    } else if (dis<0){
+        printf("\nThe Discrimant is Non-Positive\n");
+        printf("So the roots are complex");    
+    }   
+}
+int main() {
+    printf("Possible Calculations\n");
+    printf("Type the following no. to calculate\n");
+    printf("0 = calculate root of qudratic eqn\n");
+    printf("1 = calculate Determinant of matrix\n");
+    int main_select;
+    printf("Enter the No. corresponding to Calc: ");
+    scanf("%d", &main_select);
+    if (main_select == 0) {
+        printf("Root Calculations Begins\n");
+        root_of_quadraic();
+        
+    }else if (main_select == 1){
+        printf("Matric Calc Begins\n");
+        matrix();     
+    }
     return 0;
 }
